1f862318ba3d0c5ea709f4e04e5ed2220c14da6f
ENS-Yashwanth <yashwanth.erothu@vconnectech.in>
2026-08-11 16:00:15 +0530

    Updated Patch

--- main/motion_tracker.h
+++
@@ -1,9 +0,0 @@
-#ifndef MOTION_TRACKER_H
-#define MOTION_TRACKER_H
-
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-
-void motion_tracker_start(void);
-
-#endif // MOTION_TRACKER_H
--- main/sos_panic.h
+++
@@ -1,9 +0,0 @@
-#ifndef SOS_PANIC_H
-#define SOS_PANIC_H
-
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-
-void sos_panic_start(void);
-
-#endif // SOS_PANIC_H
